d635cb87ae5b851d1bb5f57a6d9a36b83ec9d7a2
webdevinfo <surajraccha55@gmail.com>
2021-05-16 22:28:09 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import store from './store';
 import ItemList from './ItemList'
+import ItemListFun from './ItemListFun'
 function App() {
     return (
         <div>
-            <ItemList/>
+            {/* <ItemList/> */}
+            <ItemListFun/>
         </div>
     )
 }
